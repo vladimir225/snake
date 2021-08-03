471f3709a50ad028499aa52b63a981fe36a75d5c
vladimir225 <47826395+vladimir225@users.noreply.github.com>
2021-08-04 00:26:29 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -11,55 +11,72 @@ const app = new Vue({
       points: new Array(8000).fill({ snake: false, eat: false }),
       path: +1,
       snake: [0],
+      oldSnake: [],
       eat: null,
-      snakeLenght: 1
+      direction: null,
+      finish: false,
+      intervalId: null
     }, 
     methods: {
       startGame() {
         this.points.splice(0, 1, { snake: true })
       },
-      test: () => console.log(1),
-      // step() {
-      //   console.log(1)
-      //   let isSnake = null;
-      //   this.points = this.points.map(item => {
-      //     if (isSnake) {
-      //       isSnake = false;
-      //       return { snake: true }
-      //     }
-      //     if (item.snake) {
-      //       isSnake = true;
-      //       return { snake: false }
-      //     } else {
-      //       return item
-      //     }
-      //   })
-      // }
       step() {
-        // this.snake[0] = this.snake[0] + this.path;
-        this.snake = this.snake.map(item => item + this.path);
+        this.oldSnake = this.snake;
+        const newSnake = this.snake.slice();
+        newSnake[0] = newSnake[0] + this.path;
+        this.snake = newSnake;
+        this.snake = this.snake.map((item, index) => {
+          if (index === 0) {
+            return item;
+          }
+
+          return this.oldSnake[index - 1];
+        })
         this.points = this.points.map((item, index) => this.snake.find(item => index === item) ? new SnakePoint(true) : index === this.eat ?  new SnakePoint(false, true) :  new SnakePoint());
 
         if (this.snake[0] === this.eat) {
           this.eatGenerate();
-          this.snakeLenght = this.snakeLenght + 1;
           this.snake.push(this.snake[this.snake.length -1] - 1);
         }
+        
+        if (typeof this.points[this.snake[0]] !== 'object' ||this.points[this.snake[0] + this.path].snake) {
+          this.finish = true;
+          clearInterval(this.intervalId);
+        }
+
+        // localStorage.setItem('data', JSON.stringify(this._data));
       },
       changeDirection(event) {
         if (event.code === 'ArrowUp') {
+          if (this.direction === 'ArrowDown') {
+            return;
+          }
           this.path = -100;
+          this.direction = event.code;
         }
 
         if (event.code === 'ArrowDown') {
+          if (this.direction === 'ArrowUp') {
+            return;
+          }
           this.path = 100;
+          this.direction = event.code;
         }
 
         if (event.code === 'ArrowLeft') {
+          if (this.direction === 'ArrowRight') {
+            return;
+          }
           this.path = -1;
+          this.direction = event.code;
         }
         if (event.code === 'ArrowRight') {
+          if (this.direction === 'ArrowLeft') {
+            return;
+          }
           this.path = 1;
+          this.direction = event.code;
         }
       },
       eatGenerate() {
@@ -69,7 +86,7 @@ const app = new Vue({
     },
     mounted() {
       this.startGame();
-      setInterval(this.step, 100);
+      this.intervalId = setInterval(this.step, 75);
       this.eatGenerate();
       document.addEventListener('keydown', this.changeDirection);
     }
